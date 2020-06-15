fbca6c6c47aedefe3fdcc128e6d85d4c1f843f91
Mohamed <mabolaumon@gmail.com>
2020-06-15 12:32:51 +0200

    Task  Background And Form (Simple Task)

---
+++ index.js
@@ -0,0 +1,4 @@
+let colors = ['blue', 'red'];
+gradient = `linear-gradient(to right ,${colors[0]} , ${colors[1]})`;
+document.body.style.backgroundImage = gradient;
+document.getElementById('form').style.minHeight = '500px'
